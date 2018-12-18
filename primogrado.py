e112574be9d6a8dd9a8a789e854b875cba51b46b
CrescenzoDeLuca <lmcdfa@gmail.com>
2018-12-18 21:24:48 +0100

    modifica

--- primogrado.py
+++ primogrado.py
@@ -2,8 +2,10 @@ a=input("inserisci a ")
 a=int(a)
 b=input("inserisci b ")
 b=int(b)
-if a==0:
-   print("x non esiste")
+if a==0 and b==0:
+   print("per ogni X")
+elif a==0:
+   print("X non esiste")
 else:
     b=-b
     x=b/a
